423b5268935f8bfe978b99e2624a42c0934f9fd5
unknown <nyshadhamsathvik@gmail.com>
2023-05-11 01:45:18 +0530

    Added risk percentage of that directory

--- raw.bat
+++ raw.bat
@@ -15,3 +15,17 @@ if not "%scanType%"=="r" if not "%scanType%"=="i" if not "%scanType%"=="f" if no
 
 "D:\ClamAv\clamscan.exe" -"%scanType%" "%directory%" > scan_results.txt
 
+REM Extract the total number of scanned files from the scan results
+for /f "tokens=2 delims=:" %%a in ('findstr /c:"Scanned files" scan_results.txt') do (
+    set /a totalFiles=%%a
+)
+
+REM Extract the total number of infected files from the scan results
+for /f "tokens=2 delims=:" %%a in ('findstr /c:"Infected files" scan_results.txt') do (
+    set /a infectedFiles=%%a
+)
+
+echo Total files scanned: %totalFiles%
+echo Infected files found: %infectedFiles%
+set /a riskScore=(%infectedFiles% * 100) / %totalFiles%
+echo The Risk Percentage of %directory% is %riskScore%
